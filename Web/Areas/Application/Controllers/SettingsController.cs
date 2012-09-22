054b5ca3087c9fd20fbe944622652d0493cf8e20
monkeyfeathers99 <monkeyfeathers99@gmail.com>
2012-09-22 18:04:14 -0400

    starting settings

---
+++ Web/Areas/Application/Controllers/SettingsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Areas.Application.Models;
+using Web.Framework;
+
+namespace Web.Areas.Application.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public ActionResult Index()
+        {
+            var identity = (BluePillIdentity)ControllerContext.HttpContext.User.Identity;
+            var list = new SelectList(identity.Collections, FindWorkingCollection(identity.Name));
+
+            var model = new SettingsModel { CollectionsDropDown = list, UserName = identity.Name };
+
+            return View(model);
+        }
+
+        public ActionResult Update(SettingsModel model)
+        {
+            var cookieName = string.Format("bluepill.{0}.preferences", model.UserName);
+            var cookies = ControllerContext.HttpContext.Request.Cookies;
+            var userCookie = cookies[cookieName];
+
+            if (userCookie == null)
+            {
+                userCookie = new HttpCookie(cookieName);
+                userCookie.Expires = DateTime.Now.AddYears(14);
+            }
+
+            userCookie.Values["wc"] = model.Collections.FirstOrDefault().ToString();
+
+            ControllerContext.HttpContext.Response.Cookies.Add(userCookie);
+
+            return RedirectToAction("index", "settings", new { Area = "application" });
+        }
+
+        private string FindWorkingCollection(string userName)
+        {
+            var cookieName = string.Format("bluepill.{0}.preferences", userName);
+            var cookies = ControllerContext.HttpContext.Request.Cookies;
+            var userCookie = cookies[cookieName];
+
+            if (userCookie != null)
+            {
+                return userCookie.Values["wc"];
+            }
+
+            return "";
+        }
+    }
+}
---
+++ Web/Scripts/settings.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+
+    $(".collectionSelect").change(function () {
+
+        $.ajax({
+            url: "/application/settings/update",
+            data: { "UserName" : "Test", "Collections" : "Whatever" },
+            success: function (result) {
+                $(".settingsContainer").empty();
+                $(".settingsContainer").append(result);
+                console.log("pass");
+            }
+        });
+
+        //$(this).closest("form").submit();
+
+    });
+
+});
