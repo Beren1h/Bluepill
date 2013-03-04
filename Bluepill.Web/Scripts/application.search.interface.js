8f3030b9bb1efca357e0d1f9d2a23b240c773388
monkeyfeathers99 <monkeyfeathers99@gmail.com>
2013-03-03 21:26:18 -0500

    ftw

--- Bluepill.Web/Scripts/application.search.interface.js
+++ Bluepill.Web/Scripts/application.search.interface.js
@@ -1,92 +1,178 @@
 ﻿
 $("document").ready(function () {
-
+   
     $(".facet-container").each(function () {
         if ($(this).data("top") == "True") {
             $(this).show("slow");
         }
     });
 
-    $(".facets-area").on("click", ".facet-action", function () {
-        $(this).siblings("ul").slideToggle(300);
-        return false;
-    });
+    //$(".facets-area").on("click", ".facet-action", function () {
+    //    $(this).siblings("ul").slideToggle(300);
+    //    return false;
+    //});
 
-    $(".facets-area").on("click", "label", function (e) {
+    $(".facet-area label").click(function (e) {
         $(e.target).toggleClass("on").toggleClass("off");
     });
 
-    $(".facets-area").on("change", "input[type=checkbox]", function (e) {
-        var area = $(".facets-area");
+    $(".accordion-body").on("shown", function () {
+        console.log("shown");
+    })
+
+    $(".facet-area input[type=checkbox]").change(function (e) {
+
+        var area = $(".facet-area");
         var target = $(e.target);
         var isChecked = target.is(":checked");
-        var facet = target.data("facet");
-        var container = area.find("#facet-" + facet);
+        var facetId = target.data("facet");
+        var container = area.find("#facet-" + facetId);
 
-        if (facet != "" && isChecked) {
-            target.closest(".facet-container").before(container);
-            container.slideToggle(200, "linear", function () {
-                $(".facet-action", container).siblings("ul").slideDown(300);
-            });
-        }
-        else if (facet != undefined && !isChecked) {
-            var child = area.find("#facet-" + facet);
 
-            if (child.length > 0) {
-                $("input[type=checkbox]", child).each(function () {
+        if (container.length > 0) {
+
+            if (isChecked) {
+                //console.log("turn it on");
+                target.closest(".facet-container").after(container.parent());
+                //console.log(target.closest(".facet-container").attr("id"));
+                //container.collapse("show");
+                //target.closest(".facet-action").trigger("click");
+                container.parent().show();
+                container.parent().find(".accordion-body").collapse("show");
+                //container.parent().slideToggle(200, function () {
+                //    $(this).find(".accordion-body").collapse("show");
+                //});
+            }
+            else {
+                //console.log("turn it off");
+                container.parent().hide();
+                var checks = container.parent().find("input[type=checkbox]");
+                checks.each(function (i) {
+
                     if ($(this).is(":checked")) {
                         $(this).attr("checked", false);
                         $("label[for=" + $(this).attr("id") + "]").trigger("click");
                         $(this).trigger("change");
                     }
+
                 });
-                child.slideToggle(300);
             }
+
         }
-        ShowSubmit();
-        SetSelection(target);
+
+
+
+
+            //var area = $(".facets-area");
+            //var target = $(e.target);
+            //var isChecked = target.is(":checked");
+            //var facet = target.data("facet");
+            //var container = area.find("#facet-" + facet);
+
+            //if (facet != "" && isChecked) {
+            //    target.closest(".facet-container").after(container);
+            //    container.show(1000, function () {
+            //        $(".facet-action", container).siblings("ul").hide(1000);
+            //    });
+            //}
+            //else if (facet != undefined && !isChecked) {
+            //    var child = area.find("#facet-" + facet);
+
+            //    if (child.length > 0) {
+            //        $("input[type=checkbox]", child).each(function () {
+            //            if ($(this).is(":checked")) {
+            //                $(this).attr("checked", false);
+            //                $("label[for=" + $(this).attr("id") + "]").trigger("click");
+            //                $(this).trigger("change");
+            //            }
+            //        });
+            //        child.show(1000);
+            //    }
+            //}
+            ////ShowSubmit();
+            //SetSelection(target);
     });
+
+    //$(".facets-area").on("click", "label", function (e) {
+    //    //$(e.target).toggleClass("on").toggleClass("off");
+    //    $(e.target).toggleClass("on");
+    //    //console.log($(e.target).attr("for"));
+    //});
+
+    //$(".facets-area").on("change", "input[type=checkbox]", function (e) {
+    //    console.log("change");
+    //    //var area = $(".facets-area");
+    //    //var target = $(e.target);
+    //    //var isChecked = target.is(":checked");
+    //    //var facet = target.data("facet");
+    //    //var container = area.find("#facet-" + facet);
+
+    //    //if (facet != "" && isChecked) {
+    //    //    target.closest(".facet-container").before(container);
+    //    //    container.slideToggle(200, "linear", function () {
+    //    //        $(".facet-action", container).siblings("ul").slideDown(300);
+    //    //    });
+    //    //}
+    //    //else if (facet != undefined && !isChecked) {
+    //    //    var child = area.find("#facet-" + facet);
+
+    //    //    if (child.length > 0) {
+    //    //        $("input[type=checkbox]", child).each(function () {
+    //    //            if ($(this).is(":checked")) {
+    //    //                $(this).attr("checked", false);
+    //    //                $("label[for=" + $(this).attr("id") + "]").trigger("click");
+    //    //                $(this).trigger("change");
+    //    //            }
+    //    //        });
+    //    //        child.slideToggle(300);
+    //    //    }
+    //    //}
+    //    //ShowSubmit();
+    //    //SetSelection(target);
+    //});
+
 });
 
-function SetSelection(target) {
-    var on = target.closest("ul").find("input[type=checkbox]").is(":checked")
+//function SetSelection(target) {
+//    var on = target.closest("ul").find("input[type=checkbox]").is(":checked")
 
-    if (on) {
-        target.closest(".facet-container").find("h3").addClass("selections");
-    }
-    else {
-        target.closest(".facet-container").find("h3").removeClass("selections");
-    }
-}
+//    if (on) {
+//        target.closest(".facet-container").find("h3").addClass("selections");
+//    }
+//    else {
+//        target.closest(".facet-container").find("h3").removeClass("selections");
+//    }
+//}
 
-function ShowSubmit() {
-    var show = $("input[type=checkbox]").is(":checked");
+//function ShowSubmit() {
+//    var show = $("input[type=checkbox]").is(":checked");
 
-    if (show) {
-        $(".submit").show();
-    }
-    else {
-        $(".submit").hide();
-    }
-};
+//    if (show) {
+//        $(".submit").show();
+//    }
+//    else {
+//        $(".submit").hide();
+//    }
+//};
 
-function ResetForm() {
-    $("input[type=checkbox]").each(function () {
-        $(this).attr("checked", false);
-        $("label[for=" + $(this).attr("id") + "]").removeClass("on").addClass("off");
-    });
+//function ResetForm() {
+//    $("input[type=checkbox]").each(function () {
+//        $(this).attr("checked", false);
+//        $("label[for=" + $(this).attr("id") + "]").removeClass("on").addClass("off");
+//    });
 
-    $(".facet-container").each(function () {
-        if ($(this).data("top") == "True") {
-            $(this).show();
-        }
-        else {
-            $(this).hide();
-        }
-        $("label", $(this)).removeClass("on").addClass("off");
-        $("h3", $(this)).removeClass("selections");
-    });
+//    $(".facet-container").each(function () {
+//        if ($(this).data("top") == "True") {
+//            $(this).show();
+//        }
+//        else {
+//            $(this).hide();
+//        }
+//        $("label", $(this)).removeClass("on").addClass("off");
+//        $("h3", $(this)).removeClass("selections");
+//    });
 
-    $(".facets-area ul").hide();
-    $(".submit").hide();
-}
+//    $(".facets-area ul").hide();
+//    $(".submit").hide();
+//}
+
--- Bluepill.Web/Scripts/bluepill.create.js
+++ Bluepill.Web/Scripts/bluepill.create.js
@@ -3,66 +3,66 @@ $("document").ready(function () {
 
     InitializeImageLoad();
 
-    $(".submit").button();
+    //$(".submit").button();
 
-    $(".submit").click(function () {
-        var data = $("form").serializeArray();
-        $(".heading img").show();
-        $(".heading span").text("saving");
-        $.post("\\bluepill\\create\\savepicture", data, function (response) {
+    //$(".submit").click(function () {
+    //    var data = $("form").serializeArray();
+    //    $(".heading img").show();
+    //    $(".heading span").text("saving");
+    //    $.post("\\bluepill\\create\\savepicture", data, function (response) {
 
-            var json = $.parseJSON(response);
+    //        var json = $.parseJSON(response);
 
-            $(".heading").data("total", json.total);
+    //        $(".heading").data("total", json.total);
 
-            if (json.total > 0) {
+    //        if (json.total > 0) {
 
-                var img = $(".add img");
-                var link = $(".add a");
-                var hidden = $("form #File");
+    //            var img = $(".add img");
+    //            var link = $(".add a");
+    //            var hidden = $("form #File");
 
-                img.css("opacity", 0);
-                img.attr("src", json.resizedSrc);
+    //            img.css("opacity", 0);
+    //            img.attr("src", json.resizedSrc);
 
-                link.attr("href", json.src);
+    //            link.attr("href", json.src);
 
-                $("form #File").val(json.file);
-                $("form #Url").val(json.url);
-            }
-            else {
-                $(".facets").remove();
-                $(".add").remove();
-                $(".heading").addClass("height12");
-            }
+    //            $("form #File").val(json.file);
+    //            $("form #Url").val(json.url);
+    //        }
+    //        else {
+    //            $(".facets").remove();
+    //            $(".add").remove();
+    //            $(".heading").addClass("height12");
+    //        }
 
-            SetHeadingCount();
-            ResetForm();
-        });
-    });
+    //        SetHeadingCount();
+    //        ResetForm();
+    //    });
+    //});
 
 });
 
 function InitializeImageLoad() {
     $(".add img").load(function () {
         $(this).animate({ opacity: 1 }, 300, function () {
-            SetHeadingCount();
+            //SetHeadingCount();
         });
     });
 }
 
-function SetHeadingCount() {
+//function SetHeadingCount() {
 
-    var total = $(".heading").data("total") 
+//    var total = $(".heading").data("total") 
 
-    $(".heading img").hide();
+//    $(".heading img").hide();
 
-    if (parseInt(total) == 0) {
-        $(".heading span").text("upload images to begin");
-        $(".add").remove();
-    }
-    else {
-        $(".heading span").text(total + " files");
-        $(".facets-area").fadeIn();
-    }
+//    if (parseInt(total) == 0) {
+//        $(".heading span").text("upload images to begin");
+//        $(".add").remove();
+//    }
+//    else {
+//        $(".heading span").text(total + " files");
+//        $(".facets-area").fadeIn();
+//    }
 
-}
+//}
