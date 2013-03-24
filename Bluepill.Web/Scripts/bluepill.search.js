90924c159e87f00f11307e47fc381400fe7e60a8
monkeyfeathers99 <monkeyfeathers99@gmail.com>
2013-03-23 23:16:12 -0400

    this works

--- Bluepill.Web/Scripts/bluepill.search.js
+++ Bluepill.Web/Scripts/bluepill.search.js
@@ -4,6 +4,28 @@
         disableScroll: false
     });
 
+    CenterThatBitch();
 
+    //help.css({ "left": target });
 
+    $(window).bind("orientationchange", function (e) {
+        CenterThatBitch();
+    });
+
+    $(window).resize(function () {
+        console.log("wtf");
+    });
 });
+
+
+function CenterThatBitch() {
+
+    var help = $(".help");
+    var target = (help.first().width() - 200) / 2;
+
+    help.each(function () {
+        var left = parseInt($(this).css("left"));
+        $(this).css({ "left": left + target });
+    });
+
+}
